904a27ad439ff54f7e6c541ae486a720fc777cbc
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2007-10-25 16:08:22 +0000

    Replaced TwillBrowser and IEBrowser with everything using selenium We may at some stage want to use twill again but for the mean time this will give us far more this currently limits the browser to firefox as selenium doesn't support discovery of browsers at the moment browser-based tests will need to be rewritten to the selenium API
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@4762 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Test/BrowserDim.py
+++ j5/src/j5/Test/BrowserDim.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python
 
-from j5.Test import IEBrowser, TwillBrowser
 from j5.Test import IterativeTester
 import sys
+import selenium
+
+# this stuff should be added to the selenium package as a patch, to support discovery of browsers
+all_browsers = ["firefox", "iexplore", "safari", "iehta", "chrome", "opera", "piiexplore", "pifirefox", "konqueror", "mock"]
+def selenium_can_find(browser_name):
+    return browser_name == "firefox"
 
 class BrowserDim(IterativeTester.Dimension):
-    def __init__(self, jsonly=False):
-        """jsonly will make a dimension only out of those browsers which support javascript"""
-        browserclasses = []
-        if TwillBrowser.UseTwill:
-            browserclasses.append(TwillBrowser.TwillBrowser)
-        if IEBrowser.UseIE:
-            browserclasses.append(IEBrowser.IEBrowser)
+    seleniumHost = "localhost"
+    seleniumPort = 4444
+    # TODO: integrate this with the web server test runner
+    browserURL = "http://localhost:8080/"
+    def __init__(self):
+        """iterates over supported browsers and runs tests on each of them"""
+        browsernames = []
         self._resources = {}
-        self._browserclasses = {}
+        self._browsernames = {}
         self._failed_conditions = {}
-        havebrowsers = False
-        for browser in browserclasses:
-            if not jsonly or browser.javascript_enabled():
+        for browser in all_browsers:
+            if selenium_can_find(browser):
                 havebrowsers = True
-                self._browserclasses[browser.__name__] = browser
-                self._resources[browser.__name__] = None
-
+                self._browsernames[browser] = browser
+                self._resources[browser] = None
+        havebrowsers = False
         if not havebrowsers:
-            self._failed_conditions["NoBrowsers"] = "No working browsers to run tests with jsonly = %r" % jsonly
+            self._failed_conditions["NoBrowsers"] = "No working browsers to run tests with"
+
+    def setup_class(cls):
+        # TODO: start selenium server automatically
+        pass
 
     def setup_method(self, browsername):
-        self._resources[browsername] = self._browserclasses[browsername]()
+        selenium_runner = selenium.selenium(self.seleniumHost, self.seleniumPort, "*%s" % browsername, self.browserURL)
+        selenium_runner.start()
+        self._resources[browsername] = selenium_runner
 
     def teardown_method(self, browsername):
-        self._resources[browsername].quit()
+        self._resources[browsername].stop()
 
 
