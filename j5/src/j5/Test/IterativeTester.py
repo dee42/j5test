888ee9e9259805b67f6c132af3a20df57070af7f
davidm <davidm@25043fe6-1616-0410-a82d-ba8f324cfe61>
2008-01-26 22:46:03 +0000

    Change all except: and excepts which catch Exception to only catch StandardError, hopefully allowing one to actually escape starting up with ctrl-c without confusing things
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@5111 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Test/IterativeTester.py
+++ j5/src/j5/Test/IterativeTester.py
@@ -145,7 +145,7 @@ class IterativeTester(object):
                     args = cls.getMethodArgs(prefix,varnames)
                     try:
                         setupmeth(*args)
-                    except Exception, e:
+                    except StandardError, e:
                         cls.iterdata['_iterativetest_setup_class_failed'] = (e,sys.exc_info()[2])
 
                     del cls.iterdata
@@ -165,7 +165,7 @@ class IterativeTester(object):
                     args = cls.getMethodArgs(prefix,varnames)
                     try:
                         teardownmeth(*args)
-                    except Exception, e:
+                    except StandardError, e:
                         cls.iterdata['_iterativetest_teardown_class_failed'] = (e,sys.exc_info()[2])
 
                     del cls.iterdata
