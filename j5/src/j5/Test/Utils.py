30cd57fd0af4bb597f981dbbd838296b060f2e9d
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2008-03-06 14:18:26 +0000

    Added a method_raises decorator that can be used like nosetest's one
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@5317 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Test/Utils.py
+++ j5/src/j5/Test/Utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from j5.Basic import Decorators
+
 def raises(ExpectedException, target, *args, **kwargs):
     """raise AssertionError, if target code does not raise the expected exception"""
     try:
@@ -9,4 +11,11 @@ def raises(ExpectedException, target, *args, **kwargs):
     except StandardError, e:
         raise AssertionError("Call to %s did not raise %s but raised %s: %s" % (target.__name__, ExpectedException.__name__, e.__class__.__name__, e))
     raise AssertionError("Call to %s did not raise %s but returned %r" % (target.__name__, ExpectedException.__name__, result))
+
+def method_raises(ExpectedException):
+    """A decorator that ensures that the underlying function raises the ExpectedException"""
+    @Decorators.decorator
+    def method_raises(target, *args, **kwargs):
+        return raises(ExpectedException, target, *args, **kwargs)
+    return method_raises
 
--- j5/src/j5/Test/test_Utils.py
+++ j5/src/j5/Test/test_Utils.py
@@ -2,6 +2,8 @@
 
 from j5.Test import Utils
 
+# some helper code for the tests
+
 class SubvalueError(ValueError):
     pass
 
@@ -15,12 +17,24 @@ def sample_method(option):
     elif option == 4:
         raise KeyError("Wrong key")
 
-def test_raises_correct():
+@Utils.method_raises(ValueError)
+def sample_decorated_function(option):
+    if option == 1:
+        return "pigeons"
+    elif option == 2:
+        raise ValueError("Wrong value")
+    elif option == 3:
+        raise SubvalueError("Wrong subvalue")
+    elif option == 4:
+        raise KeyError("Wrong key")
+
+# here are the actual tests
+def test_simple_raises_correct():
     """check raising the correct error, and a subclass of the correct error"""
     assert Utils.raises(ValueError, sample_method, 2)
     assert Utils.raises(ValueError, sample_method, 3)
 
-def test_raises_returns():
+def test_simple_raises_returns():
     """check returning a value causes an error in raises"""
     raised = None
     returned = None
@@ -34,7 +48,7 @@ def test_raises_returns():
     assert isinstance(raised, AssertionError)
     assert "Call to sample_method did not raise ValueError but returned 'pigeons'" == str(raised)
 
-def test_raises_incorrect():
+def test_simple_raises_incorrect():
     """checks that raises handles an incorrect exception properly"""
     raised = None
     returned = None
@@ -48,4 +62,38 @@ def test_raises_incorrect():
     assert isinstance(raised, AssertionError)
     print str(raised)
     assert "Call to sample_method did not raise ValueError but raised KeyError: 'Wrong key'" == str(raised)
+
+def test_method_raises_correct():
+    """check method_raises works with raising the correct error, and a subclass of the correct error"""
+    assert sample_decorated_function(2)
+    assert sample_decorated_function(3)
+
+def test_method_raises_returns():
+    """check returning a value causes an error in method_raises"""
+    raised = None
+    returned = None
+    try:
+        returned = sample_decorated_function(1)
+    except AssertionError, e:
+        raised = e
+    except StandardError, e:
+        raised = e
+    assert returned is None
+    assert isinstance(raised, AssertionError)
+    assert "Call to sample_decorated_function did not raise ValueError but returned 'pigeons'" == str(raised)
+
+def test_method_raises_incorrect():
+    """checks that method_raises handles an incorrect exception properly"""
+    raised = None
+    returned = None
+    try:
+        returned = sample_decorated_function(4)
+    except AssertionError, e:
+        raised = e
+    except StandardError, e:
+        raised = e
+    assert returned is None
+    assert isinstance(raised, AssertionError)
+    print str(raised)
+    assert "Call to sample_decorated_function did not raise ValueError but raised KeyError: 'Wrong key'" == str(raised)
 
