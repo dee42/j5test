ee88289fd800b281a2e4ca846bfca7d183520aca
davidf <davidf@25043fe6-1616-0410-a82d-ba8f324cfe61>
2008-06-05 12:47:40 +0000

    Replace terrible inspec-frame based code with yet more terrible code of the same nature The new code, however only starts up one browser per web config rather than one per test This makes the tests all run quicker...
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@5829 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Test/BrowserDim.py
+++ j5/src/j5/Test/BrowserDim.py
@@ -3,6 +3,9 @@
 from j5.Test import IterativeTester
 import sys
 import selenium
+import logging
+# FIXME: remove this once we don't do naughty things
+import inspect
 
 # this stuff should be added to the selenium package as a patch, to support discovery of browsers
 all_browsers = ["firefox", "iexplore", "safari", "iehta", "chrome", "opera", "piiexplore", "pifirefox", "konqueror", "mock"]
@@ -18,6 +21,7 @@ class BrowserDim(IterativeTester.Dimension):
         self._resources = {}
         self._browsernames = {}
         self._skipped_conditions = {}
+        self._selenium_runners = {}
         havebrowsers = False
         for browser in all_browsers:
             if selenium_can_find(browser):
@@ -27,23 +31,58 @@ class BrowserDim(IterativeTester.Dimension):
         if not havebrowsers:
             self._skipped_conditions["NoBrowsers"] = "No working browsers to run tests with"
 
-    def setup_class(cls):
+    def setup(self):
         # TODO: start selenium server automatically
-        pass
+        # FIXME: This is obviously abysmal
+        frame = inspect.currentframe()
+        # this is designed specifically for j5.Control.test_WebServer
+        web_dimension = frame.f_back.f_locals["dims"][0]
+        self.browser_urls = {}
+        for web_config_name in web_dimension.configs.keys():
+            web_config = web_dimension.configs[web_config_name]
+            app_config = web_config.get_rootnode()[web_dimension.application]
+            app_web_config = app_config.get_value(web_config.get_path(), {})
+            host = getattr(web_config, "host", "localhost")
+            port = getattr(web_config, "port", 8080)
+            baseurl = getattr(web_config, "baseurl", "")
+            if baseurl != "" and not baseurl.endswith("/"):
+                baseurl += "/"
+            app_baseurl = getattr(app_web_config, "baseurl", "")
+            baseurl += app_baseurl
+            if baseurl != "" and not baseurl.endswith("/"):
+                baseurl += "/"
+            self.browser_urls[web_config_name] = "http://%s:%s/%s" % (host, str(port), baseurl)
+
+    def start_browser(self, browsername, web_config_name):
+        """starts up a browser for the given web config"""
+        selenium_runner = selenium.selenium(self.seleniumHost, self.seleniumPort, "*%s" % browsername, self.browser_urls[web_config_name])
+        selenium_runner.start()
+        self._selenium_runners[browsername, web_config_name] = selenium_runner
+        return selenium_runner
+
+    def get_browser(self, browser_name, web_config_name):
+        """gets a controllable browser for the given browser and webserver"""
+        if (browser_name, web_config_name) in self._selenium_runners:
+            return self._selenium_runners[browser_name, web_config_name]
+        return self.start_browser(browser_name, web_config_name)
 
     def setup_method(self, browsername):
         # FIXME: provide a SANE way to pass in the config and use it to set the host and port for browserURL
-        import inspect
+        # TODO: work out a sane way to get the current web plugin dimension
         frame = inspect.currentframe()
-        # browserURL = "http://%s:%s/" % (browserHost, browserPort)
-        # this is designed specifically for j5.Control.test_WebServer
-        # FIXME: This is obviously abysmal
-        browserURL = frame.f_back.f_locals["self"].get_webtest_url()
-        selenium_runner = selenium.selenium(self.seleniumHost, self.seleniumPort, "*%s" % browsername, browserURL)
-        selenium_runner.start()
-        self._resources[browsername] = selenium_runner
+        web_config_name = frame.f_back.f_locals["varnames"][0]
+        logging.warning("Browser URL for %s is %s" % (web_config_name, self.browser_urls[web_config_name]))
+        self._resources[browsername] = self.get_browser(browsername, web_config_name)
 
     def teardown_method(self, browsername):
-        self._resources[browsername].stop()
+        # self._resources[browsername].stop()
+        pass
+
+    def teardown(self):
+        """stops any running browsers"""
+        for browsername, web_config_name in self._selenium_runners.keys():
+            selenium = self._selenium_runners.pop((browsername, web_config_name))
+            selenium.stop()
+
 
 
