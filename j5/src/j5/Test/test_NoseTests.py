ac8585feaa4aa866c57a4a92a29e49a34361f5c5
davidm <davidm@25043fe6-1616-0410-a82d-ba8f324cfe61>
2008-10-08 19:40:42 +0000

    Test our module loading on only this directory, so we don't run into problems with module setup which seems not to shutdown properly
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@6689 25043fe6-1616-0410-a82d-ba8f324cfe61

--- j5/src/j5/Test/test_NoseTests.py
+++ j5/src/j5/Test/test_NoseTests.py
@@ -3,7 +3,8 @@
 """Test module for nose tests helper"""
 
 from j5.Test import NoseTests
-import j5
+import j5.Test
+from j5.OS import ThreadControl
 
 def test_default_config():
     config = NoseTests.get_default_config()
@@ -11,7 +12,7 @@ def test_default_config():
     assert config.includeExe == True
 
 def test_get_test_loader():
-    loader = NoseTests.get_module_test_loader(j5)
+    loader = NoseTests.get_module_test_loader(j5.Test)
     suite = loader.loadTestsFromName('.')
-    assert suite.countTestCases() > 100
+    assert suite.countTestCases() > 50
 
