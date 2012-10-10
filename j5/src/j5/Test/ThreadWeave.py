6074bebf283b2c583fe76d349b063afed4da7743
davidm <davidm@25043fe6-1616-0410-a82d-ba8f324cfe61>
2012-10-10 09:25:47 +0000

    Added `only_thread` and `only_thread_blocking` conditional context managers, and some examples of how to use them (they aren't comprehensive tests...)
    git-svn-id: svn+ssh://svn.sjsoft.com/home/opensvn/svn/trunk@16180 25043fe6-1616-0410-a82d-ba8f324cfe61

---
+++ j5/src/j5/Test/ThreadWeave.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from j5.Basic import WithContextSkip
+import threading
+import logging
+
+block_event_lock = threading.Lock()
+block_events = {}
+
+@WithContextSkip.conditionalcontextmanager
+def only_thread(thread_name):
+    """Runs the controlled block only if the current thread has the given name - otherwise skips it"""
+    if thread_name == threading.current_thread().name:
+        yield
+    else:
+        raise WithContextSkip.SkipStatement()
+
+@WithContextSkip.conditionalcontextmanager
+def only_thread_blocking(thread_name, block_name):
+    """Runs the controlled block only if the current thread has the given name - otherwise skips it. Wait for the given thread to run the block before allowing other threads to proceed"""
+    with block_event_lock:
+        block_event = block_events.setdefault(block_name, threading.Event())
+    current_thread_name = threading.current_thread().name
+    if thread_name == current_thread_name:
+        logging.info("thread %s will run %s", thread_name, block_name)
+        yield
+        logging.info("thread %s ran %s", thread_name, block_name)
+        block_event.set()
+        logging.info("thread %s has set event %s", thread_name, block_name)
+    else:
+        logging.info("thread %s will wait for event %s", thread_name, block_name)
+        block_event.wait()
+        logging.info("thread %s has received event %s and will skip the block", thread_name, block_name)
+        raise WithContextSkip.SkipStatement()
+
